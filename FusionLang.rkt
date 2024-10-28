d0077a151fafce76c3548a82312fe66d71afbb39
Elkin Angulo <elkinsamir643@gmail.com>
2024-10-27 23:13:31 -0500

    Comienzo de proyecto alternativo

--- FusionLang.rkt
+++ FusionLang.rkt
@@ -1,5 +1,24 @@
+#lang eopl
+
 ; FusionLang
 
 ; Integrantes:
 ; Elkin Samir Angulo Panameño
 ; Leonardo Cuadro Lopez
+
+#|
+? Denotation Values: int, float, string, bool, proc, list, vector, dict, graph, edges y node. 
+? Expresed Values: int, float, string, bool, proc, list, vector, dict, graph, edges y node. 
+
+<program> ::= <expression> program(exp)
+
+<expression> := <int> number-int(num)
+             := <float> number-float(num)
+             := <identifier> ::= <letter> | {<letter> | 0..9}*  identifier(id)
+             := <string> string(str)
+             := <bool> ::= True | False bool(b)
+             
+
+<letter> ::= A..Z | a..z
+
+|#
